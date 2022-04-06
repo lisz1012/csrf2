ff5ef65dbcffc580cc27a9e97cdd973e18f5a826
lisz1012 <lisz1012@163.com>
2022-04-05 23:47:45 -0700

    Init

---
+++ src/main/java/com/lisz/Csrf2Application.java
@@ -0,0 +1,11 @@
+package com.lisz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csrf2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Csrf2Application.class, args);
+	}
+}
